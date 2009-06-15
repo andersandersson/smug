07a5766f4d23b85843e740ee8dd7c235f5018e3e
Anders Andersson <finrist@gmail.com>
2009-06-15 23:22:09 +0200

    Fixed the configure.ac to recognize win32 with --enable-win32 and possibility to specify GLFW-path

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -1,6 +1,7 @@
+#include "GL/glfw.h"
 #include "graphics.h"
 
 int Graphics_Init(int width, int height, char fullscreen)
 {
-  return 0;
+  return glfwOpenWindow(width, height, 8, 8, 8, 8, 24, 0, GLFW_WINDOW);
 }
--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
+#include "GL/glfw.h"
 #include "graphics/graphics.h"
 #include "physics/physics.h"
 
 int main(char argc, char* argv[])
 {
+  char running = 0;
+
+  glfwInit();
+
   Physics_Init();
   Graphics_Init(640, 480, 0);
+
+  do
+    {
+      glfwSwapBuffers();      
+
+      running = !glfwGetKey( GLFW_KEY_ESC ) &&
+	glfwGetWindowParam( GLFW_OPENED );
+    }
+  while( running );
+  
   return 0;
 }
