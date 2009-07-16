d5f851d27f63af942052c06fac257705b4a8c2c2
Mekanikles <joele375@student.liu.se>
2009-07-17 01:08:47 +0200

    tried to do some doxy-commenting

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -3,9 +3,24 @@
 
 #include "common.h"
 
+/** 
+ * Initialize Engine
+ *
+ * After this function is run, the engine is considered 
+ * up and running, even if the actual game loop has not 
+ * started yet. This state, before the gameloop has started, 
+ * should be used to load resources and set up scenes. 
+ */
 int Engine_init();
+
+/** 
+ * Close down Engine
+ */
 void Engine_terminate();
 
+/** 
+ * Run the engine, 
+ */
 void Engine_run();
 
 
--- src/graphics/graphics.h
+++ src/graphics/graphics.h
@@ -3,9 +3,29 @@
 
 #include "engine/common.h"
 
+/** 
+ * Initialize Graphics subsystem
+ *
+ * This will create a window of a given size and set it
+ * in either fullscreen or windowed mode.
+ * @param width The width of the window in pixels.
+ * @param height The height of the window in pixels.
+ * @param fullscreen Whether or not to run in fullscreen mode.
+ */
 int Graphics_init(int width, int height, BOOL fullscreen);
-void Graphics_refresh();
+
+/** 
+ * Close down Graphics subsystem
+ */
 void Graphics_terminate();
+
+/** 
+ * Refresh graphics
+ *
+ * This should be called once per visual frame.
+ */
+void Graphics_refresh();
+
 
 #endif // GRAPHICS_H
 
