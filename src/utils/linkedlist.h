68b6d30e49f9e7e22b9950e5f071d6103eaf97df
Linus Lindholm <linusonline@gmail.com>
2009-07-20 19:39:14 +0200

    More methods for LinkedList

--- src/utils/linkedlist.h
+++ src/utils/linkedlist.h
@@ -5,6 +5,8 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+#include "../engine/common.h"
+
 /** A struct for the node type in a linked list.
   *
   * Create new nodes with Node_new, delete them with Node_destroy. Set the content of the node by setting Node->item directly.
@@ -72,7 +74,54 @@ void LinkedList_addLast(LinkedList* list, void* item);
   * @param list The list.
   * @param func The function to call.
   */
-void LinkedList_traverse(LinkedList* list, void (*func)(void*));
+void LinkedList_doList(LinkedList* list, void (*func)(void*));
+
+/** A conditional doList.
+  *
+  * Same as doList, but only executes the function for those elements that satisfy the predicate function.
+  * 
+  * @relatesalso LinkedList
+  * @param list The list.
+  * @param func The function to call.
+  * @param pred The predicate function.
+  */
+void LinkedList_doListIf(LinkedList* list, void (*func)(void*), BOOL (*pred)(void*));
+
+/** Returns a new list of those elements in list matching the predicate function.
+  * 
+  * Returns a new list comprising those elements in list for which the predicate
+  * function returns true. The actual elements are note copied, only the
+  * pointers to them.
+  * 
+  * @relatesalso LinkedList
+  * @param list The list.
+  * @param pred The predicate function.
+  */
+LinkedList* LinkedList_getThose(LinkedList* list, BOOL (*pred)(void*));
+
+/** Returns a mapping of the list according to a mapping function.
+  *
+  * @relatesalso LinkedList
+  * @param list The list.
+  * @param func The mapping function.
+  */
+LinkedList* LinkedList_map(LinkedList* list, void* (*func)(void*));
+
+/** Returns true if all the list elements satisfy the predicate.
+  *
+  * @relatesalso LinkedList
+  * @param list The list.
+  * @param pred The predicate function.
+  */
+BOOL LinkedList_forAll(LinkedList* list, BOOL(*pred)(void*));
+
+/** Returns true if at least one of the list elements satisfies the predicate.
+  *
+  * @relatesalso LinkedList
+  * @param list The list.
+  * @param pred The predicate function.
+  */
+BOOL LinkedList_exists(LinkedList* list, BOOL(*pred)(void*));
 
 /** Removes the passed node from the list. Does not delete the contents of the node!
   *
