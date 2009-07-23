be14cac1374b31f6afbb7cb4f893df364fab531d
Anders Andersson (work) <anders.andersson@bilddagboken.se>
2009-07-23 09:13:03 +0200

    added a simple test

---
+++ tests/test-vector.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "physics/vector.h"
+
+int main()
+{
+    Vector u, v, w;
+
+    v = Vector_Create2d(1, 1);
+    w = Vector_Create2d(2, 3);
+
+    u = Vector_Add(v, w);
+
+    if(u.x[0] != 3 || u.x[1] != 4) {
+        printf("Vector addition failed.\n");
+        return 1;
+    }
+    
+    return 0;
+}
