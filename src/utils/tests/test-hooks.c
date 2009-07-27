5297d4fc159564d183333a2de5a8599f75dc82f1
Anders Andersson (work) <anders.andersson@bilddagboken.se>
2009-07-27 09:52:02 +0200

    added hooks structure

---
+++ src/utils/tests/test-hooks.c
@@ -0,0 +1,22 @@
+#include "hooks.h"
+
+#include <stdio.h>
+#include <assert.h>
+
+int my_func(void* x, void* y)
+{
+    return 2*(int)x+(int)y;
+}
+
+int main()
+{
+    Hook* hook = Hook_new();
+
+    hook->data = (void*) 12;
+    hook->function = my_func;
+
+    assert(29 == Hook_call(hook, (void*)5));
+
+    Hook_delete(hook);
+    return 0;
+}
