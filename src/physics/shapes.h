70ecb30b0f18d81d7ef88224b75f23702653da34
finrist <finrist@gmail.com>
2010-10-25 09:58:01 +0200

    documentation of shapes.h

--- src/physics/shapes.h
+++ src/physics/shapes.h
@@ -14,21 +14,54 @@
 #include "utils/point.h"
 #include "utils/vector.h"
 
+/** Describes the type of a Shape */
 typedef unsigned int SHAPE_TYPE;
 
 #define SHAPE_UNKNOWN   0x00
 #define SHAPE_RECTANGLE 0x01
 
+
+/** Container object for shapes
+ *
+ * A container object for different shapes. This struct will
+ * hide the different shape implementations from the user.
+ */
 typedef struct Shape
 {
+    /** The type of the shape, e.g. SHAPE_RECTANGLE */
     SHAPE_TYPE type;
+    /** Pointer to the real data for the shape */
     void* data;
 } Shape;
 
+
+
+/** Create a new Shape
+ *
+ * This will create a new empty shape object. In most cases this
+ * is not very useful, use the Shape_createFrom* instead.
+ *
+ * @relatesalso Shape
+ * @return Shape
+ */ 
 Shape* Shape_new();
 
+
+/** Free memory used by a Shape
+ * @relatesalso Shape
+ */
 void Shape_delete(Shape* shape);
 
+
+/** Create a new Shape based on a Rectangle 
+ *
+ * This function takes a Rectangle object and creates a new
+ * Shape object from it.
+ *
+ * @param rect The Rectangle to create a shape from
+ * @relatesalso Shape
+ * @return Shape
+ */
 Shape* Shape_createFromRectangle(Rectangle rect);
 
 #endif // SMUG_PHYSICS_SHAPES_H
