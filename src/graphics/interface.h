2091be39e1f4ef84fc56567a1cba506a93931ab2
Linus Lindholm <linusonline@gmail.com>
2010-12-31 13:27:53 +0100

    Expose smug types in interface.

--- src/graphics/interface.h
+++ src/graphics/interface.h
@@ -3,6 +3,9 @@
 
 /* SmugSprite* getSpriteFromFile(char* fileName); */
 
+struct Drawable;
+typedef struct Drawable* SmugDrawable;
+
 /* SmugDrawable* smugDrawable_fromSprite(SmugSprite* sprite); */
 /* SmugDrawable* smugDrawable_fromShape(SmugShape* shape); */
 /* void smugDrawable_setColor(SmugDrawable* drawable, int color); */
