f3d969894d146f83ee9a4d77e6ad8cfe4d1eb933
Linus Lindholm <linusonline@gmail.com>
2011-01-01 11:07:10 +0100

    Expose SmugColor type and related functions.

--- src/graphics/interface.c
+++ src/graphics/interface.c
@@ -1,1 +1,19 @@
 #include "interface.h"
+
+#include <graphics/drawable/box.h>
+#include <graphics/drawable/drawable.h>
+
+SmugDrawable smugDrawable_newFromRect(SmugRect rect)
+{
+    return Drawable_newBoxFromRectangle(rect);
+}
+
+void smugDrawable_setColor(SmugDrawable drawable, SmugColor color)
+{
+    Drawable_setColor(drawable, color);
+}
+
+SmugColor smugColor_create(int r, int g, int b, int a)
+{
+    return Color_createFromRGBAi(r, g, b, a);
+}
--- src/graphics/interface.h
+++ src/graphics/interface.h
@@ -1,13 +1,20 @@
 #ifndef SMUG_GRAPHICS_INTERFACE_H
 #define SMUG_GRAPHICS_INTERFACE_H
 
+#include <utils/interface.h>
+#include <graphics/color_type.h>
+
 /* SmugSprite* getSpriteFromFile(char* fileName); */
 
 struct Drawable;
 typedef struct Drawable* SmugDrawable;
 
+typedef Color SmugColor;
+
+SmugDrawable smugDrawable_newFromRect(SmugRect rect);
+void smugDrawable_setColor(SmugDrawable drawable, SmugColor color);
+SmugColor smugColor_create(int r, int g, int b, int a);
 /* SmugDrawable* smugDrawable_fromSprite(SmugSprite* sprite); */
 /* SmugDrawable* smugDrawable_fromShape(SmugShape* shape); */
-/* void smugDrawable_setColor(SmugDrawable* drawable, int color); */
 
 #endif // SMUG_GRAPHICS_INTERFACE_H
