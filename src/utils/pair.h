92110214e7c48b2bf9064874a9a1f704a5d3559e
finrist <finrist@gmail.com>
2011-01-18 21:40:28 +0100

    docuuu2

--- src/utils/pair.h
+++ src/utils/pair.h
@@ -1,13 +1,29 @@
 #ifndef SMUG_UTILS_PAIR_H
 #define SMUG_UTILS_PAIR_H
 
+/** Simple Pair struct
+  * 
+  * Not much to say. It's a Pair.
+  */
 typedef struct Pair
 {
-  void* left;
-  void* right;
+  void* left;  //< 
+  void* right; //< 
 } Pair;
 
+
+/** Create a new Pair
+ *
+ * @relatesalso Pair
+ */
 Pair* Pair_new(void);
+
+
+/** Delete a Pair
+ *
+ * @relatesalso Pair
+ * @param pair The Pair to be deleted
+ */
 void Pair_delete(Pair* pair);
 
 #endif // SMUG_UTILS_PAIR_H
